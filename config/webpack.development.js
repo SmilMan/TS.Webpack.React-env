9d1c2e80b289d23c1b013b0923644a2d6ae0f594
GYH <1490259042@qq.com>
2020-05-08 09:48:37 +0800

    com

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -3,13 +3,13 @@ var FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 var WebpackBuildNotifierPlugin = require('webpack-build-notifier');
 module.exports = {
     devServer: {
-        historyApiFallback:true,
+        historyApiFallback:true,   //需要配置，不然路由会找不到
         hot: true,
         contentBase: './dist',
     },
     plugins: [
         new FriendlyErrorsWebpackPlugin(),  //友好的错误提示信息，当出错时
-        new WebpackBuildNotifierPlugin({
+        new WebpackBuildNotifierPlugin({    //打包完时的通知
             title: "My Project Webpack Build",
             // logo: path.resolve("./img/favicon.png"),
             suppressSuccess: true
